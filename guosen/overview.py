87faae2cccb418ff084ad8a769b328663b0e265b
yumiao <18611301980@163.com>
2023-10-06 19:11:10 +0800

    加入行业信息

--- guosen/overview.py
+++ guosen/overview.py
@@ -19,16 +19,26 @@ def get_overview_data(symbol):
     response = requests.request("POST", url, headers=headers, data=payload)
     text = response.text
     result = json.loads(text)
+
+    # pb, pe
     result_sets = result["ResultSets"][6]
     result_items = result_sets["Content"]
     last_item = result_items[0]
-
     pe = last_item[0]
     pb = last_item[2]
+
+    # 行业
+    result_sets = result["ResultSets"][1]
+    result_items = result_sets["Content"]
+    last_item = result_items[0]
+    industry1 = last_item[1]
+    industry2 = last_item[2]
 
     p_item = {
         "pe": pe,
         "pb": pb,
+        "industry1": industry1,
+        "industry2": industry2
     }
     return p_item
 
